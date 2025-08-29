6446ec9fa5347b11df1ba175cd926d72345fb656
Artyom Shkuratov <@.>
2025-08-29 14:42:34 +0300

    Данные юзера

---
+++ data/users.py
@@ -0,0 +1,12 @@
+
+
+class UsersLogin:
+    STANDARD = "standard_user"
+    PROBLEM = "problem_user"
+    PERFORMANCE_GLITCH = "performance_glitch_user"
+    ERROR = "error_user"
+    VISUAL = "visual_user"
+
+class UsersPassword:
+    PASSWORD = "secret_sauce"
+    
